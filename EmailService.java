65b26e561078e6c05a0c1b4cd60cd4cea36db4fb
Rm1co <michaelee067@gmail.com>
2025-06-12 11:20:35 +0300

    Delete EmailService.java

--- EmailService.java
+++
@@ -1,8 +0,0 @@
-package dip_compliance;
-
-public class EmailService implements MessageService {
-    @Override
-    public void sendMessage(String message) {
-        System.out.println("Email sent: " + message);
-    }
-}
